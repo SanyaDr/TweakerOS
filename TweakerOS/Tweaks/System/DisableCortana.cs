ace6464f6d8344b1aa680e515394d645106c1032
Alexander <mr.drovosekov2006@yandex.ru>
2024-02-11 20:21:49 +0500

    Добавлены все системные твики

---
+++ TweakerOS/Tweaks/System/DisableCortana.cs
@@ -0,0 +1,69 @@
+using Microsoft.Win32;
+using TweakerOS.Interfaces;
+
+namespace TweakerOS.Tweaks.System;
+
+public class DisableCortana : ITweak
+{
+    public string Name => "Отключить ассистента Cortana";
+    public string Description => "Отключение Кортаны";
+
+    public bool GetIsChanged()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Enable()
+    {
+        Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search", "AllowCortana",
+            "1", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search",
+            "DisableWebSearch", "0", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search",
+            "ConnectedSearchUseWeb", "1", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search",
+            "ConnectedSearchUseWebOverMeteredConnections", "1", RegistryValueKind.DWord);
+
+        Registry.SetValue(@"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search",
+            "HistoryViewEnabled", "1", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search",
+            "DeviceHistoryEnabled", "1", RegistryValueKind.DWord);
+
+        Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search",
+            "AllowSearchToUseLocation", "1", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search",
+            "BingSearchEnabled", "1", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search", "CortanaConsent",
+            "1", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search",
+            "AllowCloudSearch", "1", RegistryValueKind.DWord);
+    }
+
+    public void Disable()
+    {
+        Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\SearchSettings",
+            "IsDeviceSearchHistoryEnabled", "0", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search", "AllowCortana",
+            "0", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search",
+            "DisableWebSearch", "1", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search",
+            "ConnectedSearchUseWeb", "0", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search",
+            "ConnectedSearchUseWebOverMeteredConnections", "0", RegistryValueKind.DWord);
+
+        Registry.SetValue(@"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search",
+            "HistoryViewEnabled", "0", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search",
+            "DeviceHistoryEnabled", "0", RegistryValueKind.DWord);
+
+        Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search",
+            "AllowSearchToUseLocation", "0", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search",
+            "BingSearchEnabled", "0", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search", "CortanaConsent",
+            "0", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search",
+            "AllowCloudSearch", "0", RegistryValueKind.DWord);
+    }
+}
---
+++ TweakerOS/Tweaks/System/WindowsStoreUpdates.cs
@@ -0,0 +1,47 @@
+using Microsoft.Win32;
+using TweakerOS.Interfaces;
+using TweakerWin.TweakHelper;
+
+namespace TweakerOS.Tweaks.System;
+
+public class WindowsStoreUpdates : ITweak
+{
+    public string Name => "Отключение обновлений Windows Store";
+    public string Description => Name;
+
+    public bool GetIsChanged()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Enable()
+    {
+        Utilities.TryDeleteRegistryValue(false, @"Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager",
+            "SilentInstalledAppsEnabled");
+        Utilities.TryDeleteRegistryValue(true, @"SOFTWARE\Policies\Microsoft\Windows\CloudContent",
+            "DisableSoftLanding");
+        Utilities.TryDeleteRegistryValue(false, @"Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager",
+            "PreInstalledAppsEnabled");
+        Utilities.TryDeleteRegistryValue(true, @"SOFTWARE\Policies\Microsoft\Windows\CloudContent",
+            "DisableWindowsConsumerFeatures");
+        Utilities.TryDeleteRegistryValue(false, @"Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager",
+            "OemPreInstalledAppsEnabled");
+        Utilities.TryDeleteRegistryValue(true, @"SOFTWARE\Policies\Microsoft\WindowsStore", "AutoDownload");
+    }
+
+    public void Disable()
+    {
+        Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager",
+            "SilentInstalledAppsEnabled", "0", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\CloudContent",
+            "DisableSoftLanding", "1", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager",
+            "PreInstalledAppsEnabled", "0", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\CloudContent",
+            "DisableWindowsConsumerFeatures", "1", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager",
+            "OemPreInstalledAppsEnabled", "0", RegistryValueKind.DWord);
+        Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsStore", "AutoDownload", "2",
+            RegistryValueKind.DWord);
+    }
+}
