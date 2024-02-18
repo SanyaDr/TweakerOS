d951d5ca201c11baa8ac3cac5ccccc7d8f8e2d8b
Alexander <mr.drovosekov2006@yandex.ru>
2024-02-18 23:00:29 +0500

    Добавлен кастомный MessageBox для показа информации о твике

---
+++ TweakerOS/View/Windows/MessageBoxCustomWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace TweakerOS.View.Windows;
+
+public partial class MessageBoxCustomWindow : Window
+{
+    public MessageBoxCustomWindow()
+    {
+        InitializeComponent();
+    }
+}
